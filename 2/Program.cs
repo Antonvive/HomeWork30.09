72a0238f3c2063ac4189e7dfddd9822ebf44030c
Anton <assddsd>
2023-09-30 19:24:05 +0500

    end

---
+++ 2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 27: Напишите программу, которая принимает на 
+//вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int Print (string mg)
+{
+    Console.Write(mg);
+    string readInput=Console.ReadLine();
+    int result=int.Parse(readInput);
+    return result;
+}
+int Sum(int N)
+{
+    int p=0;
+    int s=0;
+    while (N>0)
+    {
+        p=N%10;
+        N=N/10;
+        s=s+p;
+        
+    }
+    return s;
+}
+int number=Print("Введите положительное число:");
+Console.WriteLine($"Сумма цифр числа {number} равна: {Sum(number)}");
+
+
---
+++ 3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+//массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+int Print (string mg)
+{
+    Console.Write(mg);
+    string readInput=Console.ReadLine();
+    int result=int.Parse(readInput);
+    return result;
+}
+int[] GenerateArray(int Length, int min, int max)
+{
+    int[] array = new int[Length];
+    Random random=new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(min,max+1);
+    }
+    return array;
+}
+void PtintArray(int[] array)
+{
+    Console.Write ("[");
+    for (int i = 0; i<array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine("]");
+}
+int Length = Print("Длина массива:");
+int mina = Print ("Начальное значение, для диапазона случайного числа:");
+int maxa = Print ("Конечное значение, для диапазона случайного числа:");
+int [] array = GenerateArray(Length, mina, maxa);
+PtintArray (array);
